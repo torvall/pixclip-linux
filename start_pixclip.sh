82098ab9d5751d62ddac4daa882fe2b374e070ff
torvall <torvall@sophostech.com>
2009-08-18 00:53:56 +0100

    Fix launcher script

--- start_pixclip.sh
+++ start_pixclip.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
-cd /home/torvall/Documents/Development/PixClip/src/bin/Release
-mono ./PixClip.exe &>/dev/null
+
+#	PixClip - The easy, sleek and fast screen clipping tool (Linux version).
+#	Copyright © 2009 António Maria Torre do Valle
+#	http://www.pixclip.net
+#	
+#	
+#	This file is part of PixClip Linux.
+#
+#	PixClip Linux is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	PixClip Linux is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with PixClip Linux.  If not, see <http://www.gnu.org/licenses/>.
+
+cd `dirname $0` && mono ./PixClip.exe &>/dev/null
