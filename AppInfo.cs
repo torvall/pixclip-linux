b06b6d877f4bef177cddd85b90fe0a600abd49d6
António Maria Torre do Valle <torvall@sophostech.com>
2009-08-31 04:36:04 +0100

    Added missing source file.

---
+++ AppInfo.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace PixClip {
+
+	public static class AppInfo {
+		public const string sProgramName = "PixClip Linux";
+		public const string sVersion = "0.02";
+		public const string sComments = "Easy, sleek and fast screen clipping tool, now also on Linux!";
+		public const string sWebsite = "http://www.pixclip.net";
+		public const string sCopyright = "© 2009 António Maria Torre do Valle";
+		
+		public const string sLicense = @"PixClip Linux is licensed under the GPLv3. Read LICENSE for full text.
+
+PixClip Linux is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+PixClip Linux is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with PixClip Linux.  If not, see http://www.gnu.org/licenses/ .";
+	}
+}
